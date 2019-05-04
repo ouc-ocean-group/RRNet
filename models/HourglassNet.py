43a0a515ef1d10c7aa08e3cef41573b37fb68a30
Cynicsss <510635948@qq.com>
2019-05-04 21:52:04 +0800

    'MetricsHourglassNetConfigs'

---
+++ models/HourglassNet.py
@@ -0,0 +1,42 @@
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+from models.py_utils.kp import kp
+from models.py_utils.utils import convolution, residual
+
+def make_hg_layer(kernel, dim0, dim1, mod, layer=convolution, **kwargs):
+    layers  = [layer(kernel, dim0, dim1, stride=2)]
+    layers += [layer(kernel, dim1, dim1) for _ in range(mod - 1)]
+    return nn.Sequential(*layers)
+
+def make_pool_layer(dim):
+    return nn.Sequential()
+
+
+class HourglassNet(nn.Module):
+    def __init__(self):
+        super(HourglassNet, self).__init__()
+        self.n = 5
+        self.dims = [256, 256, 384, 384, 384, 512]
+        self.modules = [2, 2, 2, 2, 2, 4]
+
+        self.kp = kp(self.n, 1, self.dims, self.modules,
+                     make_pool_layer=make_pool_layer,
+                     make_hg_layer=make_hg_layer,
+                     kp_layer=residual, cnv_dim=256)
+
+    def forward(self, x):
+        output = self.kp(x)
+
+        return output
+
+
+
+
+if __name__ == "__main__":
+    hourglassnet = HourglassNet()
+    input = torch.randn(1, 3, 256, 256)
+    out = hourglassnet(input)
+    print(out.size())
+
+
---
+++ utils/utils.py
@@ -0,0 +1,60 @@
+import torch
+import numpy as np
+
+def xyxy2xywh(x):
+    # Convert bounding box format from [x1, y1, x2, y2] to [x, y, w, h]
+    y = torch.zeros_like(x) if isinstance(x, torch.Tensor) else np.zeros_like(x)
+    y[:, 0] = (x[:, 0] + x[:, 2]) / 2
+    y[:, 1] = (x[:, 1] + x[:, 3]) / 2
+    y[:, 2] = x[:, 2] - x[:, 0]
+    y[:, 3] = x[:, 3] - x[:, 1]
+    return y
+
+
+def xywh2xyxy(x):
+    # Convert bounding box format from [x, y, w, h] to [x1, y1, x2, y2]
+    y = torch.zeros_like(x) if isinstance(x, torch.Tensor) else np.zeros_like(x)
+    y[:, 0] = x[:, 0] - x[:, 2] / 2
+    y[:, 1] = x[:, 1] - x[:, 3] / 2
+    y[:, 2] = x[:, 0] + x[:, 2] / 2
+    y[:, 3] = x[:, 1] + x[:, 3] / 2
+    return y
+
+
+def scale_coords(img1_shape, coords, img0_shape):
+    # Rescale coords1 (xyxy) from img1_shape to img0_shape
+    gain = max(img1_shape) / max(img0_shape)  # gain  = old / new
+    coords[:, [0, 2]] -= (img1_shape[1] - img0_shape[1] * gain) / 2  # x padding
+    coords[:, [1, 3]] -= (img1_shape[0] - img0_shape[0] * gain) / 2  # y padding
+    coords[:, :4] /= gain
+    coords[:, :4] = coords[:, :4].clamp(min=0)
+    return coords
+
+
+
+
+def bbox_iou(box1, box2, x1y1x2y2=True):
+    # Returns the IoU of box1 to box2. box1 is 4, box2 is nx4
+    box2 = box2.t()
+
+    # Get the coordinates of bounding boxes
+    if x1y1x2y2:
+        # x1, y1, x2, y2 = box1
+        b1_x1, b1_y1, b1_x2, b1_y2 = box1[0], box1[1], box1[2], box1[3]
+        b2_x1, b2_y1, b2_x2, b2_y2 = box2[0], box2[1], box2[2], box2[3]
+    else:
+        # x, y, w, h = box1
+        b1_x1, b1_x2 = box1[0] - box1[2] / 2, box1[0] + box1[2] / 2
+        b1_y1, b1_y2 = box1[1] - box1[3] / 2, box1[1] + box1[3] / 2
+        b2_x1, b2_x2 = box2[0] - box2[2] / 2, box2[0] + box2[2] / 2
+        b2_y1, b2_y2 = box2[1] - box2[3] / 2, box2[1] + box2[3] / 2
+
+    # Intersection area
+    inter_area = (torch.min(b1_x2, b2_x2) - torch.max(b1_x1, b2_x1)).clamp(0) * \
+                 (torch.min(b1_y2, b2_y2) - torch.max(b1_y1, b2_y1)).clamp(0)
+
+    # Union Area
+    union_area = ((b1_x2 - b1_x1) * (b1_y2 - b1_y1) + 1e-16) + \
+                 (b2_x2 - b2_x1) * (b2_y2 - b2_y1) - inter_area
+
+    return inter_area / union_area  # iou

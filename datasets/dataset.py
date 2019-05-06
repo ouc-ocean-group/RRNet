7c7cca16b519ba912f4414e495a2062a8ae8e403
GeoffreyChen <geoffreychen777@gmail.com>
2019-05-06 13:56:53 +0800

    reformat code.

--- datasets/dataset.py
+++ datasets/dataset.py
@@ -1,21 +1,21 @@
-from __future__ import division , print_function
 import os
 import pandas as pd
 from PIL import Image
-from torch.utils.data import Dataset,DataLoader
+from torch.utils.data import Dataset, DataLoader
 import re
 import numpy as np
 
+
 class ICCVDataSet(Dataset):
-    def __init__(self,root_dir,transform=None):
+    def __init__(self, root_dir, transform=None):
         '''
         :param root_dir: root of annotations and image dirs
         :param transform: Optional transform to be applied
                 on a sample.
         '''
-        #get the csv
-        self.images_dir = os.path.join(root_dir,'images')
-        self.annotations_dir=os.path.join(root_dir,'annotations')
+        # get the csv
+        self.images_dir = os.path.join(root_dir, 'images')
+        self.annotations_dir = os.path.join(root_dir, 'annotations')
         print('create index....')
         mdf = os.listdir(self.images_dir)
         restr = r'\w+?(?=(.jpg))'
@@ -25,25 +25,22 @@ class ICCVDataSet(Dataset):
         self.mdf = mdf
         self.transform = transform
 
-
     def __len__(self):
         return len(self.mdf)
 
     def __getitem__(self, item):
-        img_name=os.path.join(self.images_dir,'{}.jpg'.format(self.mdf[item]))
-        txt_name=os.path.join(self.annotations_dir,'{}.txt'.format(self.mdf[item]))
+        img_name = os.path.join(self.images_dir, '{}.jpg'.format(self.mdf[item]))
+        txt_name = os.path.join(self.annotations_dir, '{}.txt'.format(self.mdf[item]))
         '''read image
         '''
-        image=Image.open(img_name)
+        image = Image.open(img_name)
 
         '''read annotation
         '''
-        annotation=pd.read_csv(txt_name,header=None)
-        annotation=np.array(annotation).tolist()
+        annotation = pd.read_csv(txt_name, header=None)
+        annotation = np.array(annotation).tolist()
 
-        sample = (image,annotation)
+        sample = (image, annotation)
         if self.transform:
             sample = self.transform(sample)
         return sample
-
-

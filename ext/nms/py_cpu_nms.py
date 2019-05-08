73f7d65d84866d49a1ee68389e5cd6c9da91808a
ws <471412053@qq.com>
2019-05-08 20:12:36 +0800

    ws_branch

--- ext/nms/py_cpu_nms.py
+++ ext/nms/py_cpu_nms.py
@@ -1,11 +1,5 @@
-# --------------------------------------------------------
-# Fast R-CNN
-# Copyright (c) 2015 Microsoft
-# Licensed under The MIT License [see LICENSE for details]
-# Written by Ross Girshick
-# --------------------------------------------------------
-
 import numpy as np
+
 
 def py_cpu_nms(dets, thresh):
     """Pure Python NMS baseline."""
--- ext/nms_wrapper.py
+++ ext/nms_wrapper.py
@@ -11,7 +11,6 @@ import numpy as np
 
 
 def soft_nms(dets, sigma=0.5, Nt=0.3, threshold=0.001, method=1):
-
     keep = cpu_soft_nms(np.ascontiguousarray(dets, dtype=np.float32),
                         np.float32(sigma), np.float32(Nt),
                         np.float32(threshold),

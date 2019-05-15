Fix the normalization of focal loss.

modules/loss/functional.py
@@ -31,7 +31,7 @@ def focal_loss(inputs, targets, alpha=0.25, gamma=2.0, num_classes=12, ignore=-1
     part3 = part3(inp, tar2)
     loss = part2 * part3
     loss = torch.where(tar.eq(1), (alpha * loss), ((1 - alpha) * loss))
-    loss = loss.mean()
+    loss = loss.sum()
     return loss
 
 

operators/retinanet_operator.py
@@ -97,12 +97,12 @@ class RetinaNetOperator(BaseOperator):
                 reg_preds.append(reg_pred)
                 reg_targets.append(reg_target)
 
-        cls_targets = torch.cat(cls_targets)
-        cls_loss = self.focal_loss(outs[1], cls_targets)
-
         reg_preds = torch.cat(reg_preds)
         reg_targets = torch.cat(reg_targets)
         reg_loss = F.smooth_l1_loss(reg_preds, reg_targets)
+
+        cls_targets = torch.cat(cls_targets)
+        cls_loss = self.focal_loss(outs[1], cls_targets) / reg_targets.size(0)
         return cls_loss, reg_loss
 
     def training_process(self):
1d7766f54bb1dbf5742426fe71875487fe361184
GeoffreyChen <geoffreychen777@gmail.com>
2019-05-26 20:34:08 +0800

    Init operator

--- datasets/__init__.py
+++ datasets/__init__.py
@@ -1,5 +1,5 @@
 from .drones_det import DronesDET
-from torch.utils.data import DataLoader
+from torch.utils.data import DataLoader, SubsetRandomSampler
 
 datasets = {
     'drones_det': DronesDET
@@ -24,3 +24,28 @@ def make_dataloader(cfg):
                             pin_memory=True, collate_fn=val_dataset.collate_fn,
                             shuffle=True if cfg.Train.sampler is None else False)
     return train_loader, val_loader
+
+
+def make_nas_dataloader(cfg):
+    if cfg.dataset not in datasets:
+        raise NotImplementedError
+
+    train_dataset = datasets[cfg.dataset](root_dir=cfg.data_root, transforms=cfg.Train.transforms, split='train')
+    val_dataset = datasets[cfg.dataset](root_dir=cfg.data_root, transforms=cfg.Val.transforms, split='val')
+    half_num = len(train_dataset)
+    supernet_sampler = SubsetRandomSampler(range(half_num))
+    controller_sampler = SubsetRandomSampler(range(half_num, 2 * half_num))
+
+    supernet_loader = DataLoader(train_dataset,
+                                 batch_size=cfg.Train.batch_size, num_workers=cfg.Train.num_workers,
+                                 sampler=supernet_sampler,
+                                 pin_memory=True, collate_fn=train_dataset.collate_fn)
+
+    controller_loader = DataLoader(train_dataset,
+                                   batch_size=cfg.Train.batch_size, num_workers=cfg.Train.num_workers,
+                                   sampler=controller_sampler,
+                                   pin_memory=True, collate_fn=train_dataset.collate_fn)
+    val_loader = DataLoader(val_dataset,
+                            batch_size=cfg.Val.batch_size, num_workers=cfg.Val.num_workers,
+                            pin_memory=True, collate_fn=val_dataset.collate_fn)
+    return supernet_sampler, controller_sampler, val_loader
--- utils/model_tools.py
+++ utils/model_tools.py
@@ -1,10 +1,13 @@
-from backbones.resnet import resnet50, resnet101
+from backbones.resnet import resnet10, resnet50, resnet101
 from modules.fpn import FPN
+from modules.nas.fpn import NASFPN
 from detectors.retinanet_detector import RetinaNetDetector
 
 
 def get_backbone(backbone, pretrained=False):
-    if backbone == 'resnet50':
+    if backbone == 'resnet10':
+        return resnet10(pretrained=pretrained)
+    elif backbone == 'resnet50':
         return resnet50(pretrained=pretrained)
     elif backbone == 'resnet101':
         return resnet101(pretrained=pretrained)
@@ -12,9 +15,11 @@ def get_backbone(backbone, pretrained=False):
         return resnet50(pretrained=pretrained)
 
 
-def get_fpn(fp_name):
+def get_fpn(fp_name, cfg=None):
     if fp_name == 'fpn':
         return FPN()
+    elif fp_name == 'nasfpn':
+        return NASFPN(cfg)
     else:
         return FPN()
 

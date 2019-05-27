19318ab33c33b3cea89f85c5c33f178a617c49dd
l903404160 <903404160@qq.com>
2019-05-27 09:22:51 +0800

    fix nms usage

--- ext/nms/nms_wrapper.py
+++ ext/nms/nms_wrapper.py
@@ -5,8 +5,8 @@
 # Written by Navaneeth Bodla and Bharat Singh
 # ----------------------------------------------------------
 
-from nms.gpu_nms import gpu_nms
-from nms.cpu_nms import cpu_nms, cpu_soft_nms
+from ext.nms.gpu_nms import gpu_nms
+from ext.nms.cpu_nms import cpu_nms, cpu_soft_nms
 import numpy as np
 
 
@@ -19,8 +19,33 @@ def soft_nms(dets, sigma=0.5, Nt=0.3, threshold=0.001, method=1):
 
 
 # Original NMS implementation
-def nms(dets, thresh):
+def nms(dets, thresh, gpu=False):
     if dets.shape[0] == 0:
         return []
     else:
+        if gpu:
+            return gpu_nms(dets, thresh, device_id=0)
         return cpu_nms(dets, thresh)
+
+
+if __name__ == '__main__':
+    anchor = [
+        [10, 9, 20, 19, 0.5],
+        [10, 10, 15, 30, 0.45],
+        [10, 10, 26, 26, 0.7],
+
+        [8, 9, 14, 16, 0.3],
+        [8, 8, 15, 15, 0.1],
+    ]
+    keep = soft_nms(anchor, Nt=0.4, sigma=0.3,)
+    print(keep)
+    """
+    [0, 1, 2, 3, 4]
+    # 需要看论文在仔细研究一下
+    """
+    anchor = np.array(anchor).astype(np.float32)
+    keep = nms(anchor, thresh=0.3, gpu=True)
+    print(keep)
+    """
+    [2, 3]
+    """

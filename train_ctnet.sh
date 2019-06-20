a6f4831ae6cb1ede06563ae5cf1f0dad04561cc8
GeoffreyChen <geoffreychen777@gmail.com>
2019-06-20 09:57:58 +0800

    modify shell file.

--- train_ctnet.sh
+++ train_ctnet.sh
@@ -2,30 +2,56 @@
 # Training scripts for Center Net.
 
 # Prepare for data.
+echo "=> Preparing data..."
 cd data
+echo "   Downloading data..."
 hdfs dfs -get $PAI_DEFAULT_FS_URI/data/datasets/DronesDET.tar
+
+echo "   Decompress data..."
 tar -I pigz -xf DronesDET.tar
 cd ..
 
 # Download pretrained backbone model.
+echo "=> Downloading pretrained model..."
 hdfs dfs -get $PAI_DEFAULT_FS_URI/data/models/geo/hourglass.pth
 
 # Make ext module.
+
+echo "=> Build extra module..."
 cd ext/nms
-make
+make > /dev/null 2>&1
 cd ../..
 
 # Start training.
-python3 train_ctnet.py
 
-# Save checkpoints and logs.
+echo "=> Start training..."
+if [[ $1 = "normal" ]]; then
+    python3 train_ctnet.py
+elif [[ $1 = "kl" ]]; then
+    python3 train_ctnet_kl.py
+else
+    echo "Wrong model!"
+fi
+
+# Save checkpoints a
+echo "=> Saving checkpoints and logs..."
+d logs.
 cd log
 hdfs dfs -mkdir -p $PAI_DEFAULT_FS_URI/data/models/iccvdet/CenterNet/
 for f in *; do
-    echo "Saving Checkpoint and logs... -> $f"
+    echo "   $f"
     hdfs dfs -put -f $f $PAI_DEFAULT_FS_URI/data/models/iccvdet/CenterNet/$f
 done
+cd ..
 
 # Evaluation.
-mkdir result
-python3 eval_ctnet.py
+echo "=> Start evaluating..."
+if [[ $1 = "normal" ]]; then
+    python3 eval_ctnet.py
+elif [[ $1 = "kl" ]]; then
+    python3 eval_ctnet_kl.py
+else
+    echo "Wrong model!"
+fi
+
+echo "=> All done."
---
+++ train_ctnet_kl.py
@@ -0,0 +1,8 @@
+from configs.centernet_config import Config
+from operators.centernet_kl_operator import CenterNetOperator
+
+
+if __name__ == '__main__':
+    operator = CenterNetOperator(Config)
+    operator.training_process()
+    print('Training is Done!')

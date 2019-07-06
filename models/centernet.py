f86f9e790dc06484c1d00de66e44262f2f353095
GeoffreyChen <geoffreychen777@gmail.com>
2019-07-06 17:41:54 +0800

    Fix WHConv

--- models/centernet.py
+++ models/centernet.py
@@ -2,7 +2,7 @@ import torch
 import torch.nn as nn
 from utils.model_tools import get_backbone
 from detectors.centernet_detector import CenterNetDetector
-from detectors.centernet_detector import CenterNet_WH_Detector
+from detectors.centernet_detector import CenterNetWHDetector
 
 
 class CenterNet(nn.Module):
@@ -12,7 +12,7 @@ class CenterNet(nn.Module):
         self.num_classes = cfg.num_classes
         self.backbone = get_backbone(cfg.Model.backbone, num_stacks=self.num_stacks)
         self.hm = CenterNetDetector(planes=cfg.num_classes, num_stacks=self.num_stacks, hm=True)
-        self.wh = CenterNet_WH_Detector(planes=2, num_stacks=self.num_stacks)
+        self.wh = CenterNetWHDetector(planes=2, num_stacks=self.num_stacks)
         self.reg = CenterNetDetector(planes=2, num_stacks=self.num_stacks)
 
     def forward(self, input):
